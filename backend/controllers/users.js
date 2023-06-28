41ce46481306ce43fd4768d8f48cc632d8754aee
Anton Kostylev <forestpk13@gmail.com>
2023-06-28 15:55:14 +0700

    fix

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -39,7 +39,7 @@ module.exports.login = (req, res, next) => {
     .then((user) => {
       const token = jwt.sign({ _id: user._id }, Utils.JWT_SECRET, { expiresIn: '7d' });
       res
-        .setHeader('Set-Cookie', [`token=${token}; SameSite=None, Secure; Max-Age=604800; HttpOnly`])
+        .setHeader('Set-Cookie', [`token=${token}; SameSite=None; Secure; Max-Age=604800; HttpOnly`])
         // eslint-disable-next-line spaced-comment
         /*
         .cookie('token', token, {
