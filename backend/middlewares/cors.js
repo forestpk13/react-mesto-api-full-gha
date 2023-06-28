47ccefcb94f241191f2bb7f0c86ba89af017365e
Anton Kostylev <forestpk13@gmail.com>
2023-06-28 12:22:51 +0700

    fix

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,4 +1,3 @@
-/* eslint-disable func-names */
 const allowedCors = [
   'https://forestpk13.nomoreparties.sbs/',
   'http://forestpk13.nomoreparties.sbs',
@@ -6,18 +5,23 @@ const allowedCors = [
   'http://localhost:3000',
 ];
 
+const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+
 // eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
-  const { origin } = req.headers;
   const { method } = req;
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const { origin } = req.headers;
   const requestHeaders = req.headers['access-control-request-headers'];
-
+  // для отправки credentials в fetch
+  res.header('Access-Control-Allow-Credentials', true);
+  // простой CORS
+  // проверяем, что источник запроса есть среди разрешённых
   if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', origin);
   }
-
+  // предварительный запрос
   if (method === 'OPTIONS') {
+    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
     return res.end();
