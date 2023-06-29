9da63766fbb3823c60680551c253fb29d8f0cf19
Anton Kostylev <forestpk13@gmail.com>
2023-06-29 16:35:00 +0700

    dotenv

--- backend/utils/utils.js
+++ backend/utils/utils.js
@@ -1,2 +1,3 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
 module.exports.URL_REGEXP = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/;
 module.exports.JWT_SECRET = 'a7d0e128f6ac25bc040ba4460a58328f82f5c8c314f3b4f95a086c81cecd7175';
