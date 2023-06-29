adaa54ec6821c2b631081af88a92bfa33437e141
Anton Kostylev <forestpk13@gmail.com>
2023-06-29 13:59:36 +0700

    test

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -64,7 +64,7 @@ module.exports.login = (req, res, next) => {
 };
 
 module.exports.logout = (_, res) => {
-  res.clearCookie('token', { secure: true, sameSite: 'none' }).send({ message: 'Вы вышли из профиля' });
+  res.clearCookie('token').send({ message: 'Вы вышли из профиля' });
 };
 
 const updateUser = (req, res, next, userData) => {
